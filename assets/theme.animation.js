d1255fbdbc73464de24be80df65f3a1007f93c19
phucbm <hiphucbui@gmail.com>
2020-01-04 16:17:50 +0700

    done animation for homepage

--- assets/theme.animation.js
+++ assets/theme.animation.js
@@ -3,6 +3,13 @@ EE_ANIMATION = {};
 jQuery(document).ready(function ($) {
     let $window = $(window), $body = $("body"), $html = $('html');
 
+    EE_ANIMATION.pageTransition = function () {
+        let timeline = new TimelineMax(),
+            $header = $('.ee-header');
+        timeline.to($body, .5, {opacity: 1, autoAlpha: 1}, 0);
+        timeline.from($header, .5, {opacity: 0, y: "-50%"})
+    };
+
     EE_ANIMATION.decorImages = function () {
         let controller = new ScrollMagic.Controller();
         $('.ee-decor-img').each(function () {
@@ -36,12 +43,86 @@ jQuery(document).ready(function ($) {
         });
     };
 
+    EE_ANIMATION.featuredRow = function () {
+        let controller = new ScrollMagic.Controller();
+        $('.ee-feature-row').each(function () {
+            let $wrapper = $(this), timeline = new TimelineMax(),
+                $items = $wrapper.find('.ee-feature-row__item');
+
+            timeline.staggerFrom($items, .5, {opacity: 0, y: "30%", scaleY: "1.3"}, .2, 0);
+            timeline.pause();
+
+            // Init scroll magic
+            var scene = new ScrollMagic.Scene({
+                triggerElement: this,
+                duration: $wrapper.outerHeight(),
+                triggerHook: .6
+            })
+                .addTo(controller)
+                .on("enter", function (e) {
+                    timeline.play();
+                });
+        });
+    };
+
+    EE_ANIMATION.simpleFadeIn = function () {
+        let controller = new ScrollMagic.Controller(),
+            elements = ', .ee-box-text__content';
+        $('.ee-animation-fade-in' + elements).each(function () {
+            let $this = $(this), timeline = new TimelineMax();
+
+            timeline.from($this, .5, {opacity: 0}, 0);
+            timeline.pause();
+
+            // Init scroll magic
+            var scene = new ScrollMagic.Scene({
+                triggerElement: this,
+                duration: $this.outerHeight(),
+                triggerHook: .6
+            })
+                .addTo(controller)
+                .on("enter", function (e) {
+                    timeline.play();
+                });
+        });
+    };
+
+    EE_ANIMATION.staggerFadeIn = function () {
+        let controller = new ScrollMagic.Controller(),
+            elements = ', .ee-product-grid__inner, .ee-ksp__inner, .ee-testimonials-inner, ' +
+                '.ee-box-text__txt, .ee-newsletter__inner, .ee-newsletter__content';
+        $('.ee-animation-stagger-fade-in' + elements).each(function () {
+            let $this = $(this), $items = $this.children('div'),
+                timeline = new TimelineMax();
+
+            timeline.staggerFrom($items, .5, {opacity: 0, y: '20%'}, .08, 0);
+            timeline.pause();
+
+            // Init scroll magic
+            var scene = new ScrollMagic.Scene({
+                triggerElement: this,
+                duration: $this.outerHeight(),
+                triggerHook: .7
+            })
+                .addTo(controller)
+                .on("enter", function (e) {
+                    timeline.play();
+                });
+        });
+    };
+
     /**
      * INIT FUNCTIONS
      */
     EE_ANIMATION.loadFunctions = function () {
+        EE_ANIMATION.pageTransition();
         EE_ANIMATION.decorImages();
+        EE_ANIMATION.featuredRow();
+        EE_ANIMATION.simpleFadeIn();
+        EE_ANIMATION.staggerFadeIn();
     };
 
-    EE_ANIMATION.loadFunctions();
+    $window.on('load', function () {
+        EE_ANIMATION.loadFunctions();
+    })
 });
